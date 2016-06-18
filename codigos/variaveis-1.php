2e1ad460687ba60d6e9ceb04170aeebaaae2b5c3
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2016-06-18 16:26:06 -0300

    iniciando cap. 3

---
+++ codigos/variaveis-1.php
@@ -0,0 +1,9 @@
+<?php 
+  // Declarando e atribuindo valores as variaveis
+  $nome = "Joaquim";
+  $sobrenome = "Silva";
+
+  // Imprimindo na tela os valores
+  echo "Nome: " . $nome
+  ...
+?>
---
+++ codigos/variaveis-2.php
@@ -0,0 +1,5 @@
+<?php 
+  // Exemplos de nomenclaturas
+  $codigo_cliente = "AB123";   
+  $codigoCliente = "AB123";
+?>
