7cd114f8827165df1c255cfe49666ae824fbfe1a
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2016-08-16 18:46:23 -0300

    finalizando correcoes do cap. 4

---
+++ codigos/cap4/exemplo-q10.php
@@ -0,0 +1,8 @@
+<?php
+  $s1 = “de”;
+  $s2 = “é um”;
+  $s3 = “comunicação”;
+  $s4 = “a”;
+  $s5 = “internet”;
+  $s6 = “meio”;
+?>
---
+++ codigos/cap4/exemplo-q8.php
@@ -0,0 +1,8 @@
+<?php
+  $a =10;
+  $b = 12.5;
+  $c = $a+$b;
+
+  print($a > $b? "verdadeiro": "falso");
+  print($c >= $b? "verdadeiro" : "falso");
+?>
